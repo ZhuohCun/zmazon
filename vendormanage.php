721ca9002596c58ef320e0fa9f404611535f9dca
ZhuohCun <zhuohcun@outlook.com>
2025-03-10 08:49:38 +0800

    v

---
+++ vendormanage.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <link rel="stylesheet" href="assets/payment/payment.css">
+    <title>商家管理系统</title>
+</head>
+<script>
+
+</script>
+<body>
+<div class="container">
+    <div class="left">
+        <div class="title">欢迎您，Z马逊自营商家</div>
+        <div class="item">商品管理</div>
+        <div class="item">订单管理</div>
+        <div class="item">订单管理</div>
+    </div>
+</div>
+</body>
+</html>
